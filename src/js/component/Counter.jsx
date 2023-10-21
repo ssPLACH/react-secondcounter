d2884b6d8c263dcb26103c9d188df6f7c973c09b
ssPLACH <134180679+ssPLACH@users.noreply.github.com>
2023-10-21 04:00:48 +0000

    componentes numeros

---
+++ src/js/component/Counter.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Counter = () => {
+    return (
+        <>
+            <div className="row text-white bg-dark container m-5">
+             <div className="col-2 card text-white bg-dark px-4 py-3"><h1>0</h1></div>
+             <div className="col-2 card text-white bg-dark px-4 py-3"><h1>0</h1></div>
+             <div className="col-2 card text-white bg-dark px-4 py-3"><h1>0</h1></div>
+             <div className="col-2 card text-white bg-dark px-4 py-3"><h1>0</h1></div>
+             <div className="col-2 card text-white bg-dark px-4 py-3"><h1>0</h1></div>
+             <div className="col-2 card text-white bg-dark px-4 py-3"><h1>0</h1></div>
+            </div>
+        </>
+    );
+};
+export default Counter;
